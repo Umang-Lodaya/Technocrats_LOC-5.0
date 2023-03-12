246f2f96b93af708429538877f27010617ccb679
Alistair Saldanha <shaanalistair47@gmail.com>
2023-03-12 10:03:53 +0530

    Firebase Auth

--- signup/src/Login.jsx
+++ signup/src/Login.jsx
@@ -1,8 +1,13 @@
 import './css/Login.css';
 import Normal from "./assets/normal.png"
 import Hidden from "./assets/hidden.png"
-import { useEffect, useState, useRef } from 'react';
-
+import { useState, useRef } from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth, db } from './config/firebase';
+import { addDoc, collection } from "firebase/firestore";
+import { storage } from "../src/config/firebase";
+import { ref, uploadBytes } from "firebase/storage";
+import { v4 } from 'uuid';
 
 var SignupLanding = () => {
   const invokerRef = useRef();
@@ -11,21 +16,51 @@ var SignupLanding = () => {
   const passwordBox = useRef();
   const normalEyeRef = useRef();
   const slashEyeRef = useRef();
-  
+  const [imageUpload, setImageUpload] = useState(null);
+
   function invokeFileUpload(fileTagRef) {
-    fileTagRef.current.click();        
+    fileTagRef.current.click();
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then(() => {
+      console.log("Document Uploaded!");
+    })
   }
 
   function handleEyecon(event, inputBoxRef, otherEyeRef) {
-  event.target.style.display = 'none';
-  otherEyeRef.current.style.display = 'block';
-  
-    if(event.target.className === 'eyecon normal') {
+    event.target.style.display = 'none';
+    otherEyeRef.current.style.display = 'block';
+
+    if (event.target.className === 'eyecon normal') {
       inputBoxRef.current.type = 'text';
-    } else if(event.target.className === 'eyecon slash') {
+    } else if (event.target.className === 'eyecon slash') {
       inputBoxRef.current.type = 'password';
     }
-  } 
+  }
+
+  const [userDeets, setUserDeets] = useState({});
+
+  const handleUserDetails = (event) => {
+    const { name, value } = event.target;
+    setUserDeets((prevUser) => ({ ...prevUser, [name]: value }));
+  };
+
+  const registerUser = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(auth, userDeets.email, userDeets.pwd);
+      console.log(user);
+    }
+    catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  const digitalIdentifyRef = collection(db, "digital-identify");
+
+  const createDI = async () => {
+    // await addDoc(digitalIdentifyRef,  userDeets.dob, userDeets.email, userDeets.fullName, userDeets.uid);
+    await addDoc(digitalIdentifyRef,  userDeets);
+  }
+
   return (
     <div id='main-container'>
       <div className='left-side'>
@@ -34,46 +69,46 @@ var SignupLanding = () => {
             <div className='header'>Sign in to your Account</div>
             <div className='sub-header'>Creating an account is super easy!</div>
           </div>
-        
+
           <div id='left-sub-div-2'>
             <div id='name-div' className='text-input-cn'>
-              <label htmlFor='name-input'>Full Name: </label>
-              <input type='text' id='name-input' name='name-input' className='text-input'></input>
+              <label htmlFor='fullName'>Full Name: </label>
+              <input type='text' id='name-input' name='fullName' className='text-input' value={userDeets.fullName} onChange={(e) => handleUserDetails(e)}></input>
             </div>
 
             <div id='dob-div' className='text-input-cn'>
-              <label htmlFor='dob-select'>Date of Birth: </label>
-              <input type="date" id="dob-select" name="dob-select" value="2018-07-22" />
+              <label htmlFor='dob'>Date of Birth: </label>
+              <input type="date" id="dob-select" name="dob" value={userDeets.dob} onChange={(e) => handleUserDetails(e)} />
             </div>
-            
+
             <div id='gender-div' className=''>
               <label htmlFor='gender'>Gender:</label>
               <div>
-                <label><input type="radio" id="gender-male" name="gender" value={"Male"}/>Male</label>
-                <label><input type="radio" id="gender-female" name="gender"  value={"Female"}/>Female</label>
+                <label><input type="radio" id="gender-male" name="gender" value={"Male"} onChange={(e) => handleUserDetails(e)} />Male</label>
+                <label><input type="radio" id="gender-female" name="gender" value={"Female"} onChange={(e) => handleUserDetails(e)} />Female</label>
               </div>
             </div>
 
             <div id='mobno-div' className='text-input-cn'>
-              <label htmlFor='mobno-input'>Mobile No: </label>
-              <input type='text' id='mobno-input' name='mobno-input' className='text-input'></input>
+              <label htmlFor='mobno'>Mobile No: </label>
+              <input type='text' id='mobno-input' name='mobno' className='text-input' onChange={(e) => handleUserDetails(e)}></input>
             </div>
-            
+
             <div id='pwd-div' className='text-input-cn'>
-              <label htmlFor='pwd-input'>8-digit Security Pin: </label>
-              <input type='password' ref={passwordBox} id='pwd-input' name='pwd-input' className='text-input'></input>
-              <img ref={normalEyeRef} src={Normal} alt="unhide" className='eyecon normal' onClick={(event) => {handleEyecon(event, passwordBox, slashEyeRef)}}/>
-              <img ref={slashEyeRef} src={Hidden} alt="hide" className='eyecon slash' style={{display: 'none'}} onClick={(event) => {handleEyecon(event, passwordBox, normalEyeRef)}}/>
+              <label htmlFor='pwd'>8-digit Security Pin: </label>
+              <input type='password' ref={passwordBox} id='pwd-input' name='pwd' className='text-input' onChange={(e) => handleUserDetails(e)}></input>
+              <img ref={normalEyeRef} src={Normal} alt="unhide" className='eyecon normal' onClick={(event) => { handleEyecon(event, passwordBox, slashEyeRef) }} />
+              <img ref={slashEyeRef} src={Hidden} alt="hide" className='eyecon slash' style={{ display: 'none' }} onClick={(event) => { handleEyecon(event, passwordBox, normalEyeRef) }} />
             </div>
-            
+
             <div id='email-div' className='text-input-cn'>
-              <label htmlFor='email-input'>Email ID: </label>
-              <input type='text' id='email-input' name='email-input' className='text-input'></input>
+              <label htmlFor='email'>Email ID: </label>
+              <input type='text' id='email-input' name='email' className='text-input' onChange={(e) => handleUserDetails(e)}></input>
             </div>
-            
+
             <div id='unique-id-div' className='text-input-cn'>
-              <label htmlFor='uid-input'>Identity Proof ID:</label>
-              <input type='text' id='uid-input' name='uid-input' className='text-input'></input>
+              <label htmlFor='uid'>Identity Proof ID:</label>
+              <input type='text' id='uid-input' name='uid' className='text-input' onChange={(e) => handleUserDetails(e)}></input>
             </div>
           </div>
         </div>
@@ -82,7 +117,7 @@ var SignupLanding = () => {
         <div className='right-inner'>
           <input ref={fileTagRef} type="file" id="input-file-upload" accept='image/png, image/jpg' onInput={(e) => {
             document.getElementById("uploaded-filename").innerText = e.target.files[0].name;
-          }}/>
+          }} />
           <div className='right-sub-div-1'>
             <label htmlFor='invoker'><i>Upload Identity Proof</i></label>
             <button ref={invokerRef} id="invoker" name="invoker" onClick={() => invokeFileUpload(fileTagRef)}>Upload!</button>
@@ -90,7 +125,7 @@ var SignupLanding = () => {
           </div>
         </div>
       </div>
-      <input type='submit' id='submit' value={"Submit"}></input>
+      <input type='submit' id='submit' value={"Submit"} onClick={() => { registerUser(); createDI(); setUserDeets({}) }}></input>
     </div>
   )
 }
